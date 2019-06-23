0c982c96d1d5e3444a80b73ced79f9a2db9fd6dd
LRS4 <lrspencer@hotmail.co.uk>
2019-06-23 22:11:13 +0100

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,13 +2,13 @@
 $("#title").attr("data-intro", "This portfolio displays my skills, experience, qualifications and projects.").attr("data-step", "1");
 
 // filter by subject
-$("#myBtncontent").attr("data-intro", "Select to filter by category.").attr("data-step", "2");
+$("#myBtncontent").attr("data-intro", "Select to filter panels by category.").attr("data-step", "2");
 
 // panel
 $("#sql").attr("data-intro", "Hover over a panel to view content, or click it on a mobile or tablet.").attr("data-step", "3").attr("data-scrollTo", "top");
 
 // top nav
-$("#myTopnav").attr("data-intro", "Further information can be found here.").attr("data-step", "4").attr("data-scrollTo", "top");
+$("#myTopnav").attr("data-intro", "Finally, here are links to other pages including my CV and Personal Development Plan.").attr("data-step", "4").attr("data-scrollTo", "top");
 
 // on click start intro js welcome
 $("#help").click(function() {
