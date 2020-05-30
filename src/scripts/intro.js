fa25831dc3eb6ed128e3df89fdb80b1b15443699
LRS4 <lrspencer@hotmail.co.uk>
2020-05-30 13:13:32 +0100

    Add document ready to scripts

--- src/scripts/intro.js
+++ src/scripts/intro.js
@@ -1,17 +1,19 @@
-// my portfolio
-$("#title").attr("data-intro", "This portfolio displays my skills, experience, qualifications and projects.").attr("data-step", "1");
+$(document).ready(function () {
+    // my portfolio
+    $("#title").attr("data-intro", "This portfolio displays my skills, experience, qualifications and projects.").attr("data-step", "1");
 
-// filter by subject
-$("#myBtncontent").attr("data-intro", "Select to filter panels by category.").attr("data-step", "2");
+    // filter by subject
+    $("#myBtncontent").attr("data-intro", "Select to filter panels by category.").attr("data-step", "2");
 
-// panel
-$("#sql").attr("data-intro", "Hover over a panel to view content, or click it on a mobile or tablet.").attr("data-step", "3").attr("data-scrollTo", "top");
+    // panel
+    $("#sql").attr("data-intro", "Hover over a panel to view content, or click it on a mobile or tablet.").attr("data-step", "3").attr("data-scrollTo", "top");
 
-// top nav
-$("#myTopnav").attr("data-intro", "Finally, here are links to other pages including my GitHub page and Developer Job History.").attr("data-step", "4").attr("data-scrollTo", "top");
+    // top nav
+    $("#myTopnav").attr("data-intro", "Finally, here are links to other pages including my GitHub page and Developer Job History.").attr("data-step", "4").attr("data-scrollTo", "top");
 
-// on click start intro js welcome
-$("#help").click(function () {
-    $("#showAll").trigger("click");
-    introJs().start();
+    // on click start intro js welcome
+    $("#help").click(function () {
+        $("#all").trigger("click");
+        introJs().start();
+    });
 });
