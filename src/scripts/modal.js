4977901b83f3e862ddd31eafa4d583d4cc668052
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-06-27 16:02:29 +0100

    Update about me modal to full screen overlay

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -2,7 +2,7 @@ class Modal {
     constructor() {
         this.modal = document.getElementById('myModal');
         this.btn = document.getElementById("myBtn");
-        this.span = document.getElementsByClassName("close")[0];
+        this.span = document.getElementsByClassName("closeModal")[0];
      }
 
      addEventListeners() {
@@ -10,25 +10,18 @@ class Modal {
         
         // When the user clicks the button, open the modal, show text
         this.btn.onclick = function () {
-            self.modal.style.display = "block";
+            self.modal.style.height = "100%";
         }
 
         // When the user clicks on <span> (x), close the modal
         this.span.onclick = function () {
-            self.modal.style.display = "none";
+            self.modal.style.height = "0%";
         }
 
         // When the user clicks ESC, close the model
         window.onkeyup = function (event) {
             if(event.key === "Escape") {
-                self.modal.style.display = "none";
-            }
-        }
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function (event) {
-            if (event.target == self.modal) {
-                self.modal.style.display = "none";
+                self.modal.style.height = "0%";
             }
         }
      }
