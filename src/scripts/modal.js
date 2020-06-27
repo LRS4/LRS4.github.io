3cb8fa17d482bc6a48f71ec220733f770a64d588
LRS4 <42714431+LRS4@users.noreply.github.com>
2020-06-27 16:15:35 +0100

    Add disable scroll while overlay displaying

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -8,20 +8,24 @@ class Modal {
      addEventListeners() {
         let self = this;
         
-        // When the user clicks the button, open the modal, show text
+        // When the user clicks the button, open the modal, show text, disable scroll
         this.btn.onclick = function () {
             self.modal.style.height = "100%";
+            $("body").addClass("stop-scrolling");
+            
         }
 
-        // When the user clicks on <span> (x), close the modal
+        // When the user clicks on <span> (x), close the modal, enable scroll
         this.span.onclick = function () {
             self.modal.style.height = "0%";
+            $("body").removeClass("stop-scrolling");
         }
 
-        // When the user clicks ESC, close the model
+        // When the user clicks ESC, close the model, enable scroll
         window.onkeyup = function (event) {
             if(event.key === "Escape") {
                 self.modal.style.height = "0%";
+                $("body").removeClass("stop-scrolling");
             }
         }
      }
